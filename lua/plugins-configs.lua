c3cec072bddc2b46371cf1c70bd392d974ca6f83
Khai Tran <leoalan5577@gmail.com>
2023-06-01 21:09:32 +0700

    fix: bufferline config

--- lua/plugins-configs.lua
+++ lua/plugins-configs.lua
@@ -252,4 +252,12 @@ require("indent_blankline").setup {
 -----------------------------------------------
 ----------------- BUFFERLINE-------------------
 -----------------------------------------------
-require("bufferline").setup {}
+require("bufferline").setup {
+	options = {
+		numbers = "buffer_id",
+		diagnostics = "nvim_lsp",
+		diagnostics_indicator = function(count, level, diagnostics_dict, context)
+			return "(" .. count .. ")"
+		end
+	}
+}
