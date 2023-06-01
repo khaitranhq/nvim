121b5f189f0438ef7349904a10640c6cf9a9a2cc
Khai Tran <leoalan5577@gmail.com>
2023-06-01 19:13:20 +0700

    add: bufferline and format

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,86 +1,89 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require("packer").startup(
-    function(use)
-        -- Packer can manage itself
-        use "wbthomason/packer.nvim"
+	function(use)
+		-- Packer can manage itself
+		use "wbthomason/packer.nvim"
 
-        -- Files explorer
-        use {
-            "nvim-tree/nvim-tree.lua",
-            requires = {
-                "nvim-tree/nvim-web-devicons" -- optional
-            }
-        }
+		-- Files explorer
+		use {
+			"nvim-tree/nvim-tree.lua",
+			requires = {
+				"nvim-tree/nvim-web-devicons"         -- optional
+			}
+		}
 
-        -- Theme
-        use "folke/tokyonight.nvim"
+		-- Theme
+		use "folke/tokyonight.nvim"
 
-        -- Status line
-        use {
-            "nvim-lualine/lualine.nvim",
-            requires = {
-                "nvim-tree/nvim-web-devicons",
-                opt = true
-            }
-        }
+		-- Status line
+		use {
+			"nvim-lualine/lualine.nvim",
+			requires = {
+				"nvim-tree/nvim-web-devicons",
+				opt = true
+			}
+		}
 
-        -- Git manager
-        use "tpope/vim-fugitive"
+		-- Git manager
+		use "tpope/vim-fugitive"
 
-        -- File finders
-        use {
-            "nvim-telescope/telescope.nvim",
-            tag = "0.1.1",
-            requires = {{"nvim-lua/plenary.nvim"}}
-        }
+		-- File finders
+		use {
+			"nvim-telescope/telescope.nvim",
+			tag = "0.1.1",
+			requires = { { "nvim-lua/plenary.nvim" } }
+		}
 
-        -- Install neccessary packages
-        use "williamboman/mason.nvim"
-        use "williamboman/mason-lspconfig.nvim"
+		-- Install neccessary packages
+		use "williamboman/mason.nvim"
+		use "williamboman/mason-lspconfig.nvim"
 
-        -- Configs for LSP
-        use "neovim/nvim-lspconfig" -- Collection of configurations for built-in LSP client
-        use "hrsh7th/nvim-cmp" -- Autocompletion plugin
-        use "hrsh7th/cmp-nvim-lsp" -- LSP source for nvim-cmp
-        use "saadparwaiz1/cmp_luasnip" -- Snippets source for nvim-cmp
-        use "L3MON4D3/LuaSnip" -- Snippets plugin
+		-- Configs for LSP
+		use "neovim/nvim-lspconfig"        -- Collection of configurations for built-in LSP client
+		use "hrsh7th/nvim-cmp"             -- Autocompletion plugin
+		use "hrsh7th/cmp-nvim-lsp"         -- LSP source for nvim-cmp
+		use "saadparwaiz1/cmp_luasnip"     -- Snippets source for nvim-cmp
+		use "L3MON4D3/LuaSnip"             -- Snippets plugin
 
-        -- Syntax
-        use {
-            "nvim-treesitter/nvim-treesitter",
-            run = ":TSUpdate"
-        }
+		-- Syntax
+		use {
+			"nvim-treesitter/nvim-treesitter",
+			run = ":TSUpdate"
+		}
 
-        -- Tag/pairs
-        use {
-            "windwp/nvim-autopairs",
-            config = function()
-                require("nvim-autopairs").setup {}
-            end
-        }
-        use "windwp/nvim-ts-autotag"
+		-- Tag/pairs
+		use {
+			"windwp/nvim-autopairs",
+			config = function()
+				require("nvim-autopairs").setup {}
+			end
+		}
+		use "windwp/nvim-ts-autotag"
 
-        -- Comment code
-        use "numToStr/Comment.nvim"
+		-- Comment code
+		use "numToStr/Comment.nvim"
 
-        -- Outline
-        use "simrat39/symbols-outline.nvim"
+		-- Outline
+		use "simrat39/symbols-outline.nvim"
 
-        -- Markdown Preview
-        use(
-            {
-                "iamcco/markdown-preview.nvim",
-                run = function()
-                    vim.fn["mkdp#util#install"]()
-                end
-            }
-        )
+		-- Markdown Preview
+		use(
+			{
+				"iamcco/markdown-preview.nvim",
+				run = function()
+					vim.fn["mkdp#util#install"]()
+				end
+			}
+		)
 
-        -- Indent blank line
-        use "lukas-reineke/indent-blankline.nvim"
+		-- Indent blank line
+		use "lukas-reineke/indent-blankline.nvim"
 
-        -- Some utilize short keys
-        use "tpope/vim-surround"
-    end
+		-- Some utilize short keys
+		use "tpope/vim-surround"
+
+		-- Bufferline
+		use { 'akinsho/bufferline.nvim', tag = "*", requires = 'nvim-tree/nvim-web-devicons' }
+	end
 )
