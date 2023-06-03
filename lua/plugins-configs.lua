2c4616205783d4651e1eda52cf53a88930446bd5
Khai Tran <leoalan5577@gmail.com>
2023-06-03 17:35:48 +0700

    add: cmp-path

--- lua/plugins-configs.lua
+++ lua/plugins-configs.lua
@@ -176,7 +176,8 @@ cmp.setup {
 	sources = {
 		{ name = "nvim_lsp" },
 		{ name = "luasnip" },
-		{ name = "buffer" }
+		{ name = "buffer" },
+		{ name = "path" }
 	}
 }
 
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -43,6 +43,7 @@ return require("packer").startup(
 		use "neovim/nvim-lspconfig"  -- Collection of configurations for built-in LSP client
 		use "hrsh7th/nvim-cmp"       -- Autocompletion plugin
 		use "hrsh7th/cmp-buffer"     -- Buffer words suggest
+		use "hrsh7th/cmp-path"       -- Path source for nvim-cmp
 		use "hrsh7th/cmp-nvim-lsp"   -- LSP source for nvim-cmp
 		use "saadparwaiz1/cmp_luasnip" -- Snippets source for nvim-cmp
 		use "L3MON4D3/LuaSnip"       -- Snippets plugin
