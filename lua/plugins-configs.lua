a9182b307855214c6f8445c71e3b49fb1a3be72e
Khai Tran <khai.tran@saigontechnology.com>
2023-07-25 16:43:15 +0700

    bring goto definition back

--- lua/plugins-configs.lua
+++ lua/plugins-configs.lua
@@ -118,6 +118,7 @@ require("mason-lspconfig").setup {
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
 local lspconfig = require("lspconfig")
 local navbuddy = require("nvim-navbuddy")
+local util = require "lspconfig/util"
 
 lspconfig.lua_ls.setup {
     settings = {
@@ -145,8 +146,34 @@ lspconfig.gopls.setup {
     capabilities = capabilities,
     on_attach = function(client, bufnr)
         navbuddy.attach(client, bufnr)
-    end
+    end,
+    cmd = {"gopls"},
+    filetypes = {"go", "gomod", "gowork", "gotmpl"},
+    root_dir = util.root_pattern("go.work", "go.mod", ".git"),
+    settings = {
+        gopls = {
+            completeUnimported = true
+        }
+    }
 }
+
+-- Use LspAttach autocommand to only map the following keys
+-- after the language server attaches to the current buffer
+vim.api.nvim_create_autocmd(
+    "LspAttach",
+    {
+        group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+        callback = function(ev)
+            -- Enable completion triggered by <c-x><c-o>
+            vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
+
+            -- Buffer local mappings.
+            -- See `:help vim.lsp.*` for documentation on any of the below functions
+            local opt = {buffer = ev.buf}
+            vim.keymap.set("n", "gd", vim.lsp.buf.definition, opt)
+        end
+    }
+)
 
 -- Rename variables
 keymap("n", "<leader>rn", vim.lsp.buf.rename, {noremap = true})
