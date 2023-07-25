7d4dc3f70289629d18e2f922d218492e39f47b66
Khai Tran <khai.tran@saigontechnology.com>
2023-07-25 12:26:17 +0700

    fix: remove unused config

--- lua/plugins-configs.lua
+++ lua/plugins-configs.lua
@@ -155,24 +155,6 @@ keymap("n", "<leader>rn", vim.lsp.buf.rename, {noremap = true})
 keymap("n", "[d", vim.diagnostic.goto_prev)
 keymap("n", "]d", vim.diagnostic.goto_next)
 
--- Use LspAttach autocommand to only map the following keys
--- after the language server attaches to the current buffer
-vim.api.nvim_create_autocmd(
-    "LspAttach",
-    {
-        group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-        callback = function(ev)
-            -- Enable completion triggered by <c-x><c-o>
-            vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
-
-            -- Buffer local mappings.
-            -- See `:help vim.lsp.*` for documentation on any of the below functions
-            local opt = {buffer = ev.buf}
-            vim.keymap.set("n", "gd", vim.lsp.buf.definition, opt)
-        end
-    }
-)
-
 -- luasnip setup
 local luasnip = require "luasnip"
 
