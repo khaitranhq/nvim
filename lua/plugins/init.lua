f34ceb907144f06decc4681e7f61c792fe8753ee
Khai Tran <khai.tran@saigontechnology.com>
2023-08-09 21:01:12 +0700

    add: indent blank line

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -149,6 +149,16 @@ local plugins = {
             require("Comment").setup()
         end,
         lazy = false
+    },
+    {
+        "lukas-reineke/indent-blankline.nvim",
+        config = function()
+            require("indent_blankline").setup {
+                -- for example, context is off by default, use this to turn it on
+                show_current_context = true,
+                show_current_context_start = true
+            }
+        end
     }
 }
 
