7a0a9b4a43e7e6b32dc1f1348c72908e26b869ba
Khai Tran <khai.tran@saigontechnology.com>
2023-08-09 21:23:03 +0700

    add: focus

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -169,6 +169,13 @@ local plugins = {
         config = function()
             require("hop").setup()
         end
+    },
+    {
+        "nvim-focus/focus.nvim",
+        version = "*",
+        config = function()
+            require("focus").setup()
+        end
     }
 }
 
