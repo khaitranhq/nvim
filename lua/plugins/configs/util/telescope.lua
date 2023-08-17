0d2cf0bfd7359fe8bd52c3c37620e6ef3d5564f1
Khai Tran <khai.tran@saigontechnology.com>
2023-08-17 15:41:15 +0700

    telelscope ignore patterns

--- lua/plugins/configs/util/telescope.lua
+++ lua/plugins/configs/util/telescope.lua
@@ -1,21 +1,28 @@
 require("telescope").setup {
     defaults = {
-        file_ignore_patterns = {".git", "node_modules", "dist", "venv", ".venv"}
+        file_ignore_patterns = {
+            "^node_modules/",
+            "^dist/",
+            "^venv/",
+            "^.venv/",
+            "^.git/",
+            "!^.github/"
+        }
     },
     pickers = {
         find_files = {
-            find_command = {
-                "fd",
-                "--type",
-                "file",
-                "-I",
-                "--type",
-                "symlink",
-                "--hidden",
-                "--exclude",
-                ".git"
-                -- put your other patterns here
-            },
+            -- find_command = {
+            --     "fd",
+            --     "--type",
+            --     "file",
+            --     "-I",
+            --     "--type",
+            --     "symlink",
+            --     "--hidden",
+            --     "--exclude",
+            --     ".git/*"
+            --     -- put your other patterns here
+            -- },
             hidden = true
         }
     }
