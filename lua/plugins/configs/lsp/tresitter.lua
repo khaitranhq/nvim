a600d2d1ffc55f58e43bd4060f01247c0814a8b4
Lewis Tran <leoalan5577@gmail.com>
2023-11-13 15:08:55 +0700

    fix (treesitter): enable autoinstall, disable ensure install

--- lua/plugins/configs/lsp/tresitter.lua
+++ lua/plugins/configs/lsp/tresitter.lua
@@ -1,10 +1,8 @@
 require "nvim-treesitter.configs".setup {
-    -- A list of parser names, or "all"
-    ensure_installed = {"python", "typescript", "javascript", "html", "tsx", "go"},
     -- Install parsers synchronously (only applied to `ensure_installed`)
     sync_install = true,
     -- Automatically install missing parsers when entering buffer
-    auto_install = false,
+    auto_install = true,
     -- List of parsers to ignore installing (for "all")
     -- ignore_install = { "javascript" },
 
